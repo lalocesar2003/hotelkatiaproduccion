5ea2372d38c1de57824d5ce04303cb503c2ccb16
lalocesar2003 <ceed1000@gmail.com>
2025-11-16 11:35:48 -0500

    conexion database

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ Class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=u555795455_hotel",
-						"u555795455_hotel",
-						"@Laloeselmejor5");
+		$link = new PDO("mysql:host=localhost;dbname=u555795455_dueno",
+						"u555795455_dueno",
+						"@Laloeselmejor30");
 
 		$link->exec("set names utf8");
 
