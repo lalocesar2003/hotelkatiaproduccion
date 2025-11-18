12029c5767b52537ae833f4ff04598c230e9564d
lalocesar2003 <ceed1000@gmail.com>
2025-11-18 14:56:04 -0500

    testeo youtube

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ Class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=u555795455_dueno",
-						"u555795455_dueno",
-						"sY.m5p$GMJ$t&u6");
+		$link = new PDO("mysql:host=localhost;dbname=u555795455_testeo",
+						"u555795455_testeo",
+						"@Testeoyoutube1");
 
 		$link->exec("set names utf8");
 
